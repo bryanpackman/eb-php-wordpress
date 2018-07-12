c4857bcde1f544d3133b239bd4f4e367320b339e
Bryan Packman <packman@adobe.com>
2018-07-12 17:35:30 -0400

    v1.2.0 - multisite for bryanpackman.com

--- wp-config.php
+++ wp-config.php
@@ -22,3 +22,9 @@ if ( !defined('ABSPATH') )
 require_once(ABSPATH . 'wp-settings.php');
 
 define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'bryanpackman.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
